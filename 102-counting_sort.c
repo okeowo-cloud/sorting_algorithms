d58c541d6a1f216d02b22eeb13451703f8d73d6a
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-07-17 15:13:21 -0700

    push a fix

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -11,23 +11,30 @@ void counting_sort(int *array, size_t size)
 	int maxElem = array[0];
 	int *k_array, *array1, i;
 
+	if (!array || size < 2)
+		return;
+
 	for (i = 1; i < (int)size; i++)
 	{
 		if (array[i] > maxElem)
 			maxElem = array[i];
 	}
 
-	k_array = malloc(sizeof(int) * maxElem + 1);
+	k_array = malloc(sizeof(int) * (maxElem + 1));
 	if (k_array == NULL)
 		return;
+
 	for (i = 0; i <= maxElem; i++)
 		k_array[i] = 0;
+
 	for (i = 0; i < (int)size; i++)
 		k_array[array[i]]++;
+
 	for (i = 1; i <= maxElem; i++)
 		k_array[i] += k_array[i - 1];
+
 	print_array(k_array, (size_t)maxElem + 1);
-	array1 = malloc(sizeof(int) * maxElem + 1);
+	array1 = malloc(sizeof(int) * (maxElem + 1));
 	if (array1 == NULL)
 		return;
 	for (i = size - 1; i >= 0; i--)
