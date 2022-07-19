17fac76dcd1154805abc1b1c5c83b36880de8bbd
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-07-19 13:48:02 -0700

    fixes

--- sort.h
+++ sort.h
@@ -41,7 +41,6 @@ void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
 						  size_t back);
 void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
